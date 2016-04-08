45404daded4327a59275d32f2e76da6af6c447fb
Max Harley <augeocacher@gmail.com>
2016-04-08 08:51:41 -0400

    fixed webserver stuff

--- WebServer/models/Points.js
+++ WebServer/models/Points.js
@@ -1,6 +1,5 @@
 var mongoose = require('mongoose');
 
-
 var PointsSchema = new mongoose.Schema({
   waypoint: {
     type: String,
--- WebServer/routes/index.js
+++ WebServer/routes/index.js
@@ -1,17 +1,38 @@
 var express = require('express');
 var router = express.Router();
+var Points = require('../models/Points');
 
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  var UUID = req.query.uuid;
+  if(UUID){
+    Points.find({ UUID: UUID }, function(err, points){
+      if(err){
+        console.log(err);
+        return res.status(500).send('<h1>500 Server Error</h1>');
+      }
+      return res.render('index', { points: points });
+    });
+  } else {
+    Points.find().distinct('UUID', function(err, uuids){
+      if(err){
+        console.log(err);
+        return res.status(500).send('<h1>500 Server Error</h1>');
+      }
+      console.log(uuids);
+      return res.render('index', { uuids: uuids });
+    });
+
+  }
 });
 
 router.post('/addWaypoint', function(req, res){
-  var serverPassword = process.env.APIKEY;
+  var serverPassword = process.env.APIKEY || "testpassword";
   var userPassword = req.body.APIKEY;
   
   var waypoint = req.body.waypoint;
   var time = new Date();
   var UID = req.body.UID;
+  var test = { 'waypoint': "lat, long", "UID": "theUID", "APIKEY": "123k5h213ihh"};
 
   if(serverPassword && userPassword && serverPassword === userPassword){
     Points.create({
